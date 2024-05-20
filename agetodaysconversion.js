f00ed5428f4b12818787e709c5ab280f9b980f1e
vcherubin <63928154+vcherubin@users.noreply.github.com>
2024-05-20 10:34:43 -0400

    Added /Uploaded a file to repository
    Javascript file intended to convert age to days

---
+++ agetodaysconversion.js
@@ -0,0 +1,8 @@
+// Pseudo Code to Convert Age to Days
+
+function calcAge (age) {
+	return age * 365;
+}
+	console.log(calcAge(65));
+
+// Function which takes an input number as an age and outputs the amount of days in total from that age.
